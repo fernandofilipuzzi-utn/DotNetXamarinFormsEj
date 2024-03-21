b8da65af4019a13cb15964b72e17c352e83f60db
fernandofilipuzzi-utn <fernandofilipuzzi.utn@gmail.com>
2024-03-21 12:51:39 -0300

    .

---
+++ Ej1/Ej1/Ej1/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ej1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            //string url = "https://www.google.com";// "http://desa-wallet.boleteriadigital.com.ar/validar/default";
+            string url = "https://transporteromero.boleteriadigital.com.ar/validar/login?app=true&appVersion=23";
+            Navegador.Source = url;
+        }
+    }
+}
